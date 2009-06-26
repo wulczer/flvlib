aa8c94bfa63aab1779e280d6695c3a259e290c8b
Jan Urbański <wulczer@wulczer.org>
2009-06-26 11:25:31 +0200

    Include the helpers module tests in the full library testsuite.

--- test/test_flvlib.py
+++ test/test_flvlib.py
@@ -1,10 +1,11 @@
 import unittest
-import test_primitives, test_astypes
+import test_primitives, test_astypes, test_helpers
 
 def get_suite():
-    primitives = unittest.TestLoader().loadTestsFromModule(test_primitives)
-    astypes = unittest.TestLoader().loadTestsFromModule(test_astypes)
-    return unittest.TestSuite([primitives, astypes])
+    modules = (test_primitives, test_astypes, test_helpers)
+    suites = [unittest.TestLoader().loadTestsFromModule(module) for
+              module in modules]
+    return unittest.TestSuite(suites)
 
 def main():
     unittest.TextTestRunner(verbosity=2).run(get_suite())
