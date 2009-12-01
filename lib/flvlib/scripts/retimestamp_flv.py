9bd603088aca7955d1531c318783b544007a68a0
Jan Urbański <wulczer@wulczer.org>
2009-12-01 14:42:46 +0100

    Do not fix the index in the metadata in retimestamp-flv.
    The retimestamp-flv utility should not touch anything but the
    timestamps of the FLV tags. If the index gets out of date due to
    retimestamping, the file should be reindexed.
    
    In passing change the atomic retimestamping behaviour to copy the
    whole file to a temporary location and retimestamp it in place, which
    hopefully will perform better than doing lots of small writes when
    copying tag by tag.

--- lib/flvlib/scripts/retimestamp_flv.py
+++ lib/flvlib/scripts/retimestamp_flv.py
@@ -14,6 +14,7 @@ from flvlib.primitives import make_ui8, make_ui24, make_si32_extended
 from flvlib.astypes import MalformedFLV
 from flvlib.tags import FLV, EndOfFile, AudioTag, VideoTag, ScriptTag
 from flvlib.tags import create_script_tag, create_flv_header
+from flvlib.helpers import force_remove
 
 log = logging.getLogger('flvlib.retimestamp-flv')
 
@@ -34,33 +35,7 @@ def is_nonheader_media(tag):
         return tag.h264_packet_type != H264_PACKET_TYPE_SEQUENCE_HEADER
 
 
-def offset_metadata(metadata, offset):
-    log.debug("Offsetting metadata by %d", offset)
-    try:
-        times = metadata['keyframes']['times']
-        times = [(t * 1000.0 - offset) / 1000.0 for t in times]
-        metadata['keyframes']['times'] = times
-    except TypeError:
-        # script tag variable does not support the dict protocol
-        # (is not ECMAArray nor FLVObject)
-        log.debug("Metadata variable is not indexable")
-    except KeyError:
-        # no 'keyframes' field or no 'times' field
-        log.debug("No keyframes defiinition in metadata")
-
-    return metadata
-
-
-def output_offset_metadata(fi, fo, tag, offset):
-    metadata = offset_metadata(tag.variable, offset)
-    fo.write(create_script_tag("onMetaData", metadata, tag.timestamp))
-
-
 def output_offset_tag(fi, fo, tag, offset):
-    if (isinstance(tag, ScriptTag) and tag.name == "onMetaData"):
-        output_offset_metadata(fi, fo, tag, offset)
-        return
-
     new_timestamp = tag.timestamp - offset
     # do not offset non-media and media header
     if not is_nonheader_media(tag):
@@ -80,100 +55,11 @@ def output_offset_tag(fi, fo, tag, offset):
     fo.write(fi.read(tag.size + 7))
 
 
-def retimestamp_tags_atomically(fi, fo, flv):
-    offset = None
-    queue = []
-
-    # Wait for the first nonheader media tag, queue other tags before that.
-    # After getting it, flush the queue and start outputting offset tags.
-    for tag in flv.iter_tags():
-        if offset is None and is_nonheader_media(tag):
-            # set the offset, write the header and flush the queue
-            offset = tag.timestamp
-            log.debug("Determined the offset to be %d", offset)
-            fo.write(create_flv_header(has_audio=flv.has_audio,
-                                       has_video=flv.has_video))
-            for queued in queue:
-                output_offset_tag(fi, fo, queued, offset)
-            queue = []
-
-        if offset is None:
-            # no offset yet, queue the tag
-            queue.append(tag)
-        else:
-            output_offset_tag(fi, fo, tag, offset)
-
-    # if we haven't managed to determine the offset, assume it's 0 and
-    # flush the queue (it will be empty if we managed to find the offset)
-    for queued in queue:
-        output_offset_tag(fi, fo, queued, 0)
-
-
-def retimestamp_file_atomically(inpath, outpath):
-    try:
-        f = open(inpath, 'rb')
-        fi = open(inpath, 'rb')
-    except IOError, (errno, strerror):
-        log.error("Failed to open `%s': %s", inpath, strerror)
-        return False
-
-    if outpath:
-        try:
-            fo = open(outpath, 'wb')
-        except IOError, (errno, strerror):
-            log.error("Failed to open `%s': %s", outpath, strerror)
-            return False
-    else:
-        try:
-            fd, temppath = tempfile.mkstemp()
-            # preserve the permission bits
-            shutil.copymode(inpath, temppath)
-            fo = os.fdopen(fd, 'wb')
-        except EnvironmentError, (errno, strerror):
-            log.error("Failed to create temporary file: %s", strerror)
-            return False
-
+def retimestamp_tags_inplace(f, fu):
     flv = FLV(f)
-
-    try:
-        retimestamp_tags_atomically(fi, fo, flv)
-    except IOError, (errno, strerror):
-        log.error("Failed to create the retimestamped file: %s", strerror)
-        return False
-    except MalformedFLV, e:
-        message = e[0] % e[1:]
-        log.error("The file `%s' is not a valid FLV file: %s", inpath, message)
-        return False
-    except EndOfFile:
-        log.error("Unexpected end of file on file `%s'", inpath)
-        return False
-
-    f.close()
-    fi.close()
-    fo.close()
-
-    if not outpath:
-        # If we were not writing directly to the output file
-        # we need to overwrite the original
-        try:
-            shutil.move(temppath, inpath)
-        except EnvironmentError, (errno, strerror):
-            log.error("Failed to overwrite the original file "
-                      "with the indexed version: %s", strerror)
-            return False
-
-    return True
-
-
-def retimestamp_tags_inplace(fu, flv):
     offset = None
-    metadata_tag = None
 
     for tag in flv.iter_tags():
-        # remember the metadata tag to be able to update it later
-        if isinstance(tag, ScriptTag) and tag.name == "onMetaData":
-            metadata_tag = tag
-
         if offset is None and is_nonheader_media(tag):
             offset = tag.timestamp
             log.debug("Determined the offset to be %d", offset)
@@ -182,19 +68,6 @@ def retimestamp_tags_inplace(fu, flv):
         if offset is not None and offset != 0:
             fu.seek(tag.offset + 4, os.SEEK_SET)
             fu.write(make_si32_extended(tag.timestamp - offset))
-
-    # if there was no onMetaData tag or no nonheader media, we're done
-    if not metadata_tag or offset is None:
-        return
-
-    # offset the timestamps in the metadata
-    metadata = offset_metadata(metadata_tag.variable, offset)
-
-    # seek to the metadata position and write out the changed metadata
-    # note that the size of the metadata will always be unchanged, since we're
-    # just changing the values of some Number fields
-    fu.seek(metadata_tag.offset, os.SEEK_SET)
-    fu.write(create_script_tag("onMetaData", metadata, metadata_tag.timestamp))
 
 
 def retimestamp_file_inplace(inpath):
@@ -205,10 +78,8 @@ def retimestamp_file_inplace(inpath):
         log.error("Failed to open `%s': %s", inpath, strerror)
         return False
 
-    flv = FLV(f)
-
     try:
-        retimestamp_tags_inplace(fu, flv)
+        retimestamp_tags_inplace(f, fu)
     except IOError, (errno, strerror):
         log.error("Failed to create the retimestamped file: %s", strerror)
         return False
@@ -222,6 +93,74 @@ def retimestamp_file_inplace(inpath):
 
     f.close()
     fu.close()
+
+    return True
+
+
+def retimestamp_file_atomically(inpath, outpath):
+    try:
+        f = open(inpath, 'rb')
+    except IOError, (errno, strerror):
+        log.error("Failed to open `%s': %s", inpath, strerror)
+        return False
+
+    if outpath:
+        try:
+            fo = open(outpath, 'w+b')
+        except IOError, (errno, strerror):
+            log.error("Failed to open `%s': %s", outpath, strerror)
+            return False
+    else:
+        try:
+            fd, temppath = tempfile.mkstemp()
+            # preserve the permission bits
+            shutil.copymode(inpath, temppath)
+            fo = os.fdopen(fd, 'wb')
+        except EnvironmentError, (errno, strerror):
+            log.error("Failed to create temporary file: %s", strerror)
+            return False
+
+    try:
+        shutil.copyfileobj(f, fo)
+    except EnvironmentError, (errno, strerror):
+        log.error("Failed to create temporary copy: %s", strerror)
+        force_remove(temppath)
+        return False
+
+    f.seek(0)
+    fo.seek(0)
+
+    try:
+        retimestamp_tags_inplace(f, fo)
+    except IOError, (errno, strerror):
+        log.error("Failed to create the retimestamped file: %s", strerror)
+        if not outpath:
+            force_remove(temppath)
+        return False
+    except MalformedFLV, e:
+        message = e[0] % e[1:]
+        log.error("The file `%s' is not a valid FLV file: %s", inpath, message)
+        if not outpath:
+            force_remove(temppath)
+        return False
+    except EndOfFile:
+        log.error("Unexpected end of file on file `%s'", inpath)
+        if not outpath:
+            force_remove(temppath)
+        return False
+
+    f.close()
+    fo.close()
+
+    if not outpath:
+        # If we were not writing directly to the output file
+        # we need to overwrite the original
+        try:
+            shutil.move(temppath, inpath)
+        except EnvironmentError, (errno, strerror):
+            log.error("Failed to overwrite the original file "
+                      "with the indexed version: %s", strerror)
+            return False
 
     return True
 
@@ -242,12 +181,10 @@ def process_options():
     usage = "%prog [-i] [-U] file [outfile|file2 file3 ...]"
     description = (
 """Rewrites timestamps in FLV files making by the first media tag timestamped
-    with 0. The rest of the tags is retimestamped relatively. If the file has a
-    onMetaData script tag with an index, it will get updated accordingly as
-    well. With the -i (inplace) option modifies the files without creating
-    temporary copies. With the -U (update) option operates on all parameters,
-    updating the files in place. Without the -U option accepts one input and
-    one output file path.
+    with 0. The rest of the tags is retimestamped relatively. With the -i
+    (inplace) option modifies the files without creating temporary copies. With
+    the -U (update) option operates on all parameters, updating the files in
+    place. Without the -U option accepts one input and one output file path.
 """)
     version = "%%prog flvlib %s" % __versionstr__
     parser = OptionParser(usage=usage, description=description,
